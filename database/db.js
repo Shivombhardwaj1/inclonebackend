adced2db9fa2f90290773fa72a086a073e359d43
shivombhardwaj1 <shivombhardwaj3156@gmail.com>
2023-08-07 20:54:54 +0530

    first commit

--- database/db.js
+++ database/db.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
-const Connection = async (username,password) => {
-    const URL =  `mongodb://${username}:${password}@ac-tctgi5e-shard-00-00.fdujv2k.mongodb.net:27017,ac-tctgi5e-shard-00-01.fdujv2k.mongodb.net:27017,ac-tctgi5e-shard-00-02.fdujv2k.mongodb.net:27017/?ssl=true&replicaSet=atlas-d7hzic-shard-0&authSource=admin&retryWrites=true&w=majority`
+const Connection = async (URL) => {
 
     try {   
 
--- server.js
+++ server.js
@@ -8,15 +8,19 @@ const app= express();
 dotenv.config();
 app.use(cors());
 app.use('/', Route);
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static("client/build"));
+    
+}
 
 
-
-const PORT=8000;
+const PORT=process.env.PORT || 8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
+const URL = process.env.MONGODB_URI || `mongodb://${username}:${password}@ac-tctgi5e-shard-00-00.fdujv2k.mongodb.net:27017,ac-tctgi5e-shard-00-01.fdujv2k.mongodb.net:27017,ac-tctgi5e-shard-00-02.fdujv2k.mongodb.net:27017/?ssl=true&replicaSet=atlas-d7hzic-shard-0&authSource=admin&retryWrites=true&w=majority`
 
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
-Connection(username,password);
+Connection(URL);
 
 
 DefaultData();
